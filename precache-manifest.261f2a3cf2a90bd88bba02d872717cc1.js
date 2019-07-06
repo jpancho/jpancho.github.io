a7618303fac274a4b9cf74bc015648cc09ad05f9
jpancho <jpancho@sfu.ca>
2019-07-06 01:25:29 -0700

    Updates

---
+++ precache-manifest.261f2a3cf2a90bd88bba02d872717cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36655101a123a6ca96b0c0e6add9a07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c9404168e43a92d7235",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0c9404168e43a92d7235",
+    "url": "/static/js/main.522e2176.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
