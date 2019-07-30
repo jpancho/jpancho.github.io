8d29c8e73b05f62b4913a4c6762e09c6cf1e87f4
jpancho <jpancho@sfu.ca>
2019-07-30 14:44:43 -0700

    Change title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./components/navbar";
-import PaperSheet from "./components/profile";
-import Experience from "./components/experience";
 
 function App() {
   return (
